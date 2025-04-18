62194bb42c85d7572726a50244f5a041b9fe4066
cumachenko <kirito22122005@gmail.com>
2025-04-18 08:43:28 +0300

    Implement string classification by case in console application

--- Practice_11/Program.cs
+++ Practice_11/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> inputList = new List<string>();
+        List<string> uppercaseList = new List<string>();
+        List<string> notUppercaseList = new List<string>();
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введіть рядки (для завершення введіть порожній рядок):");
+        while (true)
+        {
+            Console.Write("Введіть рядок: ");
+            string? line = Console.ReadLine();
+            if (string.IsNullOrEmpty(line))
+                break;
+            inputList.Add(line);
+        }
+
+        foreach (var str in inputList)
+        {
+            if (IsAllUpper(str))
+                uppercaseList.Add(str);
+            else
+                notUppercaseList.Add(str);
+        }
+
+        Console.WriteLine("\nСписок рядків, що містять ТІЛЬКИ ВЕЛИКІ літери:");
+        foreach (var str in uppercaseList)
+            Console.WriteLine(str);
+
+        Console.WriteLine("\nСписок рядків, що містять НЕ ТІЛЬКИ великі літери:");
+        foreach (var str in notUppercaseList)
+            Console.WriteLine(str);
+    }
+    
+    static bool IsAllUpper(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return false;
+        foreach (char c in s)
+        {
+            if (!char.IsUpper(c))
+                return false;
+        }
+        return true;
+    }
+}
